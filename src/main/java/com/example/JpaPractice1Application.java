d205c8241c10519f2825c27ee479b2c8b8586e73
vmannn <vicochia@pdx.edu>
2021-01-12 10:20:28 -0800

    testing

--- src/main/java/com/example/JpaPractice1Application.java
+++ src/main/java/com/example/JpaPractice1Application.java
@@ -3,9 +3,14 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class JpaPractice1Application {
 
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JpaPractice1Application.class, args);
 	}
