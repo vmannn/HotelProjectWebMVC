eaa5d2f8db1ecd4cae670d1ba0f2083ead9d5384
vmannn <vicochia@pdx.edu>
2021-01-11 22:33:09 -0800

    added tomcat dependency for jenkins

--- src/main/java/com/example/controller/HotelRoomController.java
+++ src/main/java/com/example/controller/HotelRoomController.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
